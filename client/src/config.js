f3cd0f323d84af332b8ea5e1aa421d6c0ad2beb9
ylorant <yohann.lorant@gmail.com>
2021-01-08 12:54:16 +0100

    Remove and ignore the configuration file (samples to come soon)

--- client/src/config.js
+++
@@ -1,9 +0,0 @@
-export default {
-    api: {
-        host: 'http://' + window.location.hostname + ':8080'
-    },
-    listener: {
-        hubUrl: "http://127.0.0.1:8082/.well-known/mercure",
-        topic: "launchpad"
-    }
-};
